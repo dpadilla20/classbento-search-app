7df12d09c29acc0487a9322784eae11e603a7ad2
Daniel Padilla <dpadillavalentin@gmail.com>
2021-10-19 19:22:20 -0500

    add animation card

--- src/common/Spinner.js
+++ src/common/Spinner.js
@@ -1,10 +1,9 @@
 import React from "react";
-import './../assets/spinner.css';
 
 export const Spinner = () => {
   return (
-    <div class=" flex justify-center items-center mt-5">
-      <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-red-900"></div>
+    <div className=" flex justify-center items-center mt-5">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-900"></div>
     </div>
   );
 };
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Card = ({ title, link, image, description, author, dateTaken, tags }) => {
   return (
-    <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
+    <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2 animate__animated animate__fadeIn">
       <div className="card">
         <a href={link} target="_blank" rel="noreferrer">
          <img src={image} alt="" className="w-full" />
@@ -17,15 +17,15 @@ export const Card = ({ title, link, image, description, author, dateTaken, tags 
         </div>
         <div className="px-6 py-4">
           {
-            tags.split(" ").map((tag) => { 
-              return <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2">
+            tags.split(" ").map((tag, index) => { 
+              return <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2">
                 #{ tag }
               </span>
             })
           }
         </div>
-        <div class="p-4 text-right text-sm text-gray-600">
-          <span class="ml-2">By: { author }</span>
+        <div className="p-4 text-right text-sm text-gray-600">
+          <span className="ml-2">By: { author }</span>
         </div>
         {/* <footer className="blockquote-footer">By: { author }</footer> */}
       </div>
